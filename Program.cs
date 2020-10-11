ff0e3dad203a1a6b07633e1fdfeeb83ef334173c
Maciej Rudnicki <mrrudnicki@wp.pl>
2020-10-11 11:20:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bloc_concurrency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
